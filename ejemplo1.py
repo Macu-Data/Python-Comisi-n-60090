4bd9b03f5204efeba2fed353b5e163654a32446a
Macu-Data <maculus32@gmail.com>
2024-11-11 22:26:39 -0300

    testing

--- ejemplo1.py
+++ ejemplo1.py
@@ -4,4 +4,9 @@ def saludando():
 
     def sumar (n1,n2):
         print("el resultado de la suma de n1 y n2 es de: ", n1 + n2)
-        sumar(int(input("ingresa el primer numero: ")),int(input("ingrese el segundo numero: ")))       
+        sumar(int(input("ingresa el primer numero: ")),int(input("ingrese el segundo numero: ")))  
+
+    def dividir (n1,n2):
+        print("el resultado de la division de n1 y n2 es de: ", n1 / n2)
+        dividir(int(input("ingresa el primer numero: ")),int(input("ingrese el segundo numero: "))) 
+    
